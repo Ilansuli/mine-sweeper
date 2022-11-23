af064b860dbba0fed100d083b4fb34f1840a380e
Ilansuli <97478676+Ilansuli@users.noreply.github.com>
2022-11-23 21:00:43 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,31 +5,34 @@
 const MINE = '💣'
 const MARK = '🚩'
 const SMILEY = '😎'
+const LIFE = '💖'
 
 var gBoard
-
-    function onInIt() {
-        gBoard = createBoard(4, 4)
-        renderBoard(gBoard)
-    }
+var gCounter = 0
 
 
-function createBoard(rows, cols) {
+function onInIt() {
+    gBoard = buildBoard(6)
+    // console.log(gBoard);
+    renderBoard(gBoard)
+    renderTools()
+}
+
+function buildBoard(size) {
     var board = []
 
-    for (var i = 0; i < rows; i++) {
-        board.push([])
-        for (var j = 0; j < cols; j++) {
-            if (Math.random() > 0.2) {
-
-                board[i][j]= {
+    for (let i = 0; i < size; i++) {
+        board[i] = []
+        for (let j = 0; j < size; j++) {
+            if (Math.random() > 0.3) {
+                board[i][j] = {
                     minesAroundCount: 4,
                     isShown: false,
                     isMine: false,
                     isMarked: true
                 }
             } else {
-                board[i][j]= {
+                board[i][j] = {
                     minesAroundCount: 4,
                     isShown: false,
                     isMine: true,
@@ -41,14 +44,25 @@ function createBoard(rows, cols) {
     return board
 }
 
+function onCellClicked(elCell){
 
-function onCellClicked(elCell) {
-    var i = elCell.dataset.i
+    var i = elCell.dataset.i  
     var j = elCell.dataset.j
-    if (!gBoard[i][j].isMine) {
-        elCell.innerText = countNegs(i,j,gBoard);
-    }else{
-        elCell.innerText = MINE
+    if(!gBoard[i][j].isShown){
+        gBoard[i][j].isShown = true
+        elCell.style.contentVisibility= "visible"
+    }
+    checkGameOver(i,j)
+}
+
+function checkGameOver(i,j){
+    if(gBoard[i][j].isMine && gBoard[i][j].isShown){
+        gLifeCounter--
+        renderBoard(gBoard)
+        if(gLifeCounter === 0){
+            console.log('GAME');
+        }
+
     }
 }
 
--- js/utiles.js
+++ js/utiles.js
@@ -1,33 +1,53 @@
-function renderBoard(board){
-    var strHTML =''
+
+function renderBoard(board) {
+    var strHTML = ''
     strHTML += `<tbody>`
     for (let i = 0; i < board.length; i++) {
         strHTML += `<tr>`
         for (let j = 0; j < board[0].length; j++) {
-            var cell
             var cellData = 'data-i="' + i + '" data-j="' + j + '"'
             var cellClass = `cell-${i}-${j}`
-            var cell = (board[i][j].isMine) ? '' : ''
-            strHTML += `<td ${cellData} class ="cell ${cellClass}" onclick="onCellClicked(this)">${cell}</td>`
 
+            var cell = (board[i][j].isMine) ? MINE : setMinesNegsCount(gBoard, i, j)
+
+            strHTML += `<td  ${cellData} class ="cell ${cellClass}" onclick="onCellClicked(this)">${cell}</td>`
         }
         strHTML += `</tr>`
     }
     strHTML += `</tbody>`
+    // document.querySelector(.cell)
 
     return document.querySelector('.board').innerHTML = strHTML
 }
 
-function countNegs(cellI, cellJ, mat) {
-    var negsMinesCount = 0
-    for (var i = cellI - 1; i <= cellI + 1; i++) {
-        if (i < 0 || i >= mat.length) continue
-        for (var j = cellJ - 1; j <= cellJ + 1; j++) {
-            if (i === cellI && j === cellJ) continue
-            if (j < 0 || j >= mat[i].length) continue
-            if (mat[i][j].isMine) negsMinesCount++
+function setMinesNegsCount(board, rowIdx, colIdx) {
+    var MinesCount = 0
+    for (var i = rowIdx - 1; i <= rowIdx + 1; i++) {
+        if (i < 0 || i >= board.length) continue
+        for (var j = colIdx - 1; j <= colIdx + 1; j++) {
+            if (i === rowIdx && j === colIdx) continue
+            if (j < 0 || j >= board[0].length) continue
+            // var currCell = board[i][j]
+            if (board[i][j].isMine) MinesCount++
         }
     }
-    // console.log(negsMinesCount);
-    return negsMinesCount
+    return MinesCount
+}
+
+function renderTools(){
+ var elOptionsTools =   document.querySelector('.options-tool')
+ var elLife = elOptionsTools.querySelector('.life')
+ var elSmiley = elOptionsTools.querySelector('.smiley')
+ elSmiley.innerText = SMILEY
+ elLife.innerText = [LIFE,LIFE,LIFE]
+}
+function showTimer(){
+
+
+}
+
+function scoreCounter(){
+
+
+
 }
